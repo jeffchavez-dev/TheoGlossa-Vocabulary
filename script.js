61a1e3c5023bcf2576f49b0f7f1e84ac1bd99909
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-04-12 21:34:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -33,17 +33,21 @@ const setVocabulary = () => {
         let i = 0;
         const img = document.createElement('img')
         img.src = quiz[0].image
-        vocabs.appendChild(img)
+        
 
         const vocab = `
-            <div class="vocabulary"></div>
+            <div class="vocabulary">
+                <img src="${quiz[0].image}">
+                </div>
             <div class="choices">
-                <div class="a choice"></div>
-                <div class="b choice"></div>
-                <div class="c choice"></div>
-                <div class="d choice"></div>
+                <div class="a choice">${quiz[0].a}</div>
+                <div class="b choice">${quiz[0].b}</div>
+                <div class="c choice">${quiz[0].c}</div>
+                <div class="d choice">${quiz[0].d}</div>
             </div>        
         `
+
+        vocabs.innerHTML = vocab
 
         nextImg.addEventListener('click', () => {
             i++
@@ -55,14 +59,14 @@ const setVocabulary = () => {
 
 setVocabulary()
 
-const setChoices = () => {
-    const allchoices = quiz.map((choice) => {
-        a.innerHTML = choice.a
-        b.innerHTML = choice.b
-        c.innerHTML = choice.c
-        d.innerHTML = choice.d
-    })
-}
+// const setChoices = () => {
+//     const allchoices = quiz.map((choice) => {
+//         a.innerHTML = choice.a
+//         b.innerHTML = choice.b
+//         c.innerHTML = choice.c
+//         d.innerHTML = choice.d
+//     })
+// }
 
 setChoices()
 
