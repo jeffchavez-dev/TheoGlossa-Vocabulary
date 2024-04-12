6fe827d9c45647cc1a82c1e4f257d9675b3d5817
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-04-12 15:44:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,11 @@ const choices = document.querySelector('.choices')
 const quiz = [
     {
         image: "/Assets/αὐτός.png",
-        choices: 
-            {
-                a: "αὐτός",
-                b: "αὐτή",
-                c: "σύ",
-                d: "ἐγώ",
-                answer: "αὐτός"
-            }
+        a: "αὐτός",
+        b: "αὐτή",
+        c: "σύ",
+        d: "ἐγώ",
+        answer: "αὐτός"
        
     }
 
