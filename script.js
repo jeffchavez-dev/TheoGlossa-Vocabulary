27a66161bbbea0f384e7f281860a7e4ba7921472
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-04-15 12:18:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ const setVocabulary = () => {
                     setNext()
                 } else {
                     alert('οὐκ')
-                    
+                    document.getElementById("status").innerText = 'οὐκ'
                     console.log(choice.innerText)
                 }
 
