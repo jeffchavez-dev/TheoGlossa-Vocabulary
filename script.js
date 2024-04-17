a4d17861cf27f3d3f0374d236077e79c87d580cb
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-04-17 18:09:06 +0800

    add

--- script.js
+++ script.js
@@ -16,149 +16,202 @@ const showCheck = document.querySelector('.check')
 const showCross = document.querySelector('.cross')
 
 const quiz = [
-    {
-        image: "/Assets/αὐτός.png",
-        a: "αὐτός",
-        b: "αὐτή",
-        c: "σύ",
-        d: "ἐγώ",
-        answer: "αὐτός"
-    },
-    {
-        image: "/Assets/ἐγώ.png",
-        a: "αὐτός",
-        b: "αὐτή",
-        c: "σύ",
-        d: "ἐγώ",
-        answer: "ἐγώ"
-    },
-    {
-        image: "/Assets/σύ.png",
-        a: "αὐτός",
-        b: "αὐτή",
-        c: "σύ",
-        d: "ἐγώ",
-        answer: "σύ"
-    },
-    {
-        image: "/Assets/αὐτή.png",
-        a: "αὐτός",
-        b: "αὐτή",
-        c: "σύ",
-        d: "ἐγώ",
-        answer: "αὐτή"
-    },
-    {
-        image: "/Assets/μέγας.png",
-        a: "μικρός",
-        b: "μέγας",
-        c: "κακός",
-        d: "καλός",
-        answer: "μέγας"
-    },
-    {
-        image: "/Assets/μικρός.png",
-        a: "κακός",
-        b: "καλός",
-        c: "μέγας",
-        d: "μικρός",
-        answer: "μικρός"
-    },
-    {
-        image: "/Assets/ἡ γυνή.png",
-        a: "ἡ ὄρνις",
-        b: "ἡ γυνή",
-        c: "ὁ ἀλέκτωρ",
-        d: "ὁ ἀνήρ",
-        answer: "ἡ γυνή"
-    },
-    {
-        image: "/Assets/ὁ ἀνήρ.png",
-        a: "ὁ ἀνήρ",
-        b: "ἡ ὄρνις",
-        c: "ἡ γυνή",
-        d: "ὁ ἀλέκτωρ",
-        answer: "ὁ ἀνήρ"
-    },
-    {
-        image: "/Assets/ἡ ὄρνις.png",
-        a: "ὁ ἀνήρ",
-        b: "ἡ ὄρνις",
-        c: "ὁ ἀλέκτωρ",
-        d: "ἡ γυνή",
-        answer: "ἡ ὄρνις"
-    },
-    {
-        image: "/Assets/ὁ ἀλέκτωρ.png",
-        a: "ὁ ἀνήρ",
-        b: "ἡ οἰκία",
-        c: "ἡ γυνή",
-        d: "ὁ ἀλέκτωρ",
-        answer: "ὁ ἀλέκτωρ"
-    },
-    {
-        image: "/Assets/ἡ ὑδρία.png",
-        a: "ὁ ἄνθρωπος",
-        b: "ὁ ἵππος",
-        c: "ἡ ὑδρία",
-        d: "ὁ ἀλέκτωρ",
-        answer: "ἡ ὑδρία"
-    },
-    {
-        image: "/Assets/ἡ οἰκία.png",
-        a: "ὁ ἀνήρ",
-        b: "ἡ οἰκία",
-        c: "ὁ ἄνθρωπος",
-        d: "ὁ ἀλέκτωρ",
-        answer: "ἡ οἰκία"
-    },
-    {
-        image: "/Assets/ὁ ἄνθρωπος.png",
-        a: "ὁ ἄνθρωπος",
-        b: "ἡ οἰκία",
-        c: "ὁ κύων",
-        d: "ὁ ἀλέκτωρ",
-        answer: "ὁ ἄνθρωπος"
-    },
-    {
-        image: "/Assets/ὁ ἵππος.png",
-        a: "ὁ ἀνήρ",
-        b: "ἡ οἰκία",
-        c: "ὁ βοῦς",
-        d: "ὁ ἵππος",
-        answer: "ὁ ἵππος"
-    },
-    {
-        image: "/Assets/ὁ βοῦς.png",
-        a: "ὁ ἄνθρωπος",
-        b: "ὁ βοῦς",
-        c: "ὁ ἵππος",
-        d: "ὁ κύων",
-        answer: "ὁ βοῦς"
-    },
-    {
-        image: "/Assets/ὁ κύων.png",
-        a: "ὁ κύων",
-        b: "ἡ οἰκία",
-        c: "ἡ γυνή",
-        d: "ὁ ἵππος",
-        answer: "ὁ κύων"
-    }       
+ 
 ]
- // {
-    //     image: "",
-    //     a: "",
-    //     b: "",
-    //     c: "",
-    //     d: "",
-    //     answer: ""
-    // }
 
 
-// chooseLessons.addEventListener('click', () => {
-//     const quizSection = document.querySelector('.quiz-section')
-//     quizSection.style.display = block
-// })
+const allVocabulary = [
+        { "Vocabulary 1": [ 
+            {
+                image: "/Assets/αὐτός.png",
+                a: "αὐτός",
+                b: "αὐτή",
+                c: "σύ",
+                d: "ἐγώ",
+                answer: "αὐτός"
+            },
+            {
+                image: "/Assets/ἐγώ.png",
+                a: "αὐτός",
+                b: "αὐτή",
+                c: "σύ",
+                d: "ἐγώ",
+                answer: "ἐγώ"
+            },
+            {
+                image: "/Assets/σύ.png",
+                a: "αὐτός",
+                b: "αὐτή",
+                c: "σύ",
+                d: "ἐγώ",
+                answer: "σύ"
+            },
+            {
+                image: "/Assets/αὐτή.png",
+                a: "αὐτός",
+                b: "αὐτή",
+                c: "σύ",
+                d: "ἐγώ",
+                answer: "αὐτή"
+            },
+            {
+                image: "/Assets/μέγας.png",
+                a: "μικρός",
+                b: "μέγας",
+                c: "κακός",
+                d: "καλός",
+                answer: "μέγας"
+            },
+            {
+                image: "/Assets/μικρός.png",
+                a: "κακός",
+                b: "καλός",
+                c: "μέγας",
+                d: "μικρός",
+                answer: "μικρός"
+            },
+            {
+                image: "/Assets/ἡ γυνή.png",
+                a: "ἡ ὄρνις",
+                b: "ἡ γυνή",
+                c: "ὁ ἀλέκτωρ",
+                d: "ὁ ἀνήρ",
+                answer: "ἡ γυνή"
+            },
+            {
+                image: "/Assets/ὁ ἀνήρ.png",
+                a: "ὁ ἀνήρ",
+                b: "ἡ ὄρνις",
+                c: "ἡ γυνή",
+                d: "ὁ ἀλέκτωρ",
+                answer: "ὁ ἀνήρ"
+            },
+            {
+                image: "/Assets/ἡ ὄρνις.png",
+                a: "ὁ ἀνήρ",
+                b: "ἡ ὄρνις",
+                c: "ὁ ἀλέκτωρ",
+                d: "ἡ γυνή",
+                answer: "ἡ ὄρνις"
+            },
+            {
+                image: "/Assets/ὁ ἀλέκτωρ.png",
+                a: "ὁ ἀνήρ",
+                b: "ἡ οἰκία",
+                c: "ἡ γυνή",
+                d: "ὁ ἀλέκτωρ",
+                answer: "ὁ ἀλέκτωρ"
+            },
+            {
+                image: "/Assets/ἡ ὑδρία.png",
+                a: "ὁ ἄνθρωπος",
+                b: "ὁ ἵππος",
+                c: "ἡ ὑδρία",
+                d: "ὁ ἀλέκτωρ",
+                answer: "ἡ ὑδρία"
+            },
+            {
+                image: "/Assets/ἡ οἰκία.png",
+                a: "ὁ ἀνήρ",
+                b: "ἡ οἰκία",
+                c: "ὁ ἄνθρωπος",
+                d: "ὁ ἀλέκτωρ",
+                answer: "ἡ οἰκία"
+            },
+            {
+                image: "/Assets/ὁ ἄνθρωπος.png",
+                a: "ὁ ἄνθρωπος",
+                b: "ἡ οἰκία",
+                c: "ὁ κύων",
+                d: "ὁ ἀλέκτωρ",
+                answer: "ὁ ἄνθρωπος"
+            },
+            {
+                image: "/Assets/ὁ ἵππος.png",
+                a: "ὁ ἀνήρ",
+                b: "ἡ οἰκία",
+                c: "ὁ βοῦς",
+                d: "ὁ ἵππος",
+                answer: "ὁ ἵππος"
+            },
+            {
+                image: "/Assets/ὁ βοῦς.png",
+                a: "ὁ ἄνθρωπος",
+                b: "ὁ βοῦς",
+                c: "ὁ ἵππος",
+                d: "ὁ κύων",
+                answer: "ὁ βοῦς"
+            },
+            {
+                image: "/Assets/ὁ κύων.png",
+                a: "ὁ κύων",
+                b: "ἡ οἰκία",
+                c: "ἡ γυνή",
+                d: "ὁ ἵππος",
+                answer: "ὁ κύων"
+            }      
+         ]
+        },
+        { "Vocabulary 2": [ 
+            
+            {
+                image: "/Assets/σύ.png",
+                a: "αὐτός",
+                b: "αὐτή",
+                c: "σύ",
+                d: "ἐγώ",
+                answer: "σύ"
+            },
+            {
+                image: "/Assets/αὐτή.png",
+                a: "αὐτός",
+                b: "αὐτή",
+                c: "σύ",
+                d: "ἐγώ",
+                answer: "αὐτή"
+            },
+            {
+                image: "/Assets/μέγας.png",
+                a: "μικρός",
+                b: "μέγας",
+                c: "κακός",
+                d: "καλός",
+                answer: "μέγας"
+            }]
+        }
+           
+]
+
+let myQuiz = []
+
+let j = 0;
+const setNewVocabulary = document.querySelectorAll('.lessons')
+setNewVocabulary.forEach(newVocabulary => {
+    newVocabulary.addEventListener('click', () => {
+        const clickedLesson = newVocabulary.innerText
+
+       // Iterate over each object in allVocabulary
+
+       allVocabulary.forEach(vocabularyObj => {
+        const keys = Object.keys(vocabularyObj);
+        const matchingKey = keys.find(key => 
+            clickedLesson === key
+        )
+        if (matchingKey) {
+               
+                console.log(`Array for ${clickedLesson}:`, vocabularyObj[matchingKey]);
+                vocabularyObj[matchingKey].forEach(obj => {
+                    myQuiz.push(obj);
+                });
+                console.log(`my quiz: ${JSON.stringify(myQuiz)}`);
+                setVocabulary()
+            } 
+       })  
+    }
+        
+    )
+})
+
 
 
 let i = 0; 
@@ -172,11 +225,14 @@ function shuffleArray(array) {
     }
     return array;
   }
-const shuffledArray = shuffleArray(quiz)
+const shuffledArray = shuffleArray(myQuiz)
 
 const setVocabulary = () => {
+        console.log('i:', i);
+        console.log('shuffledArray:', shuffledArray);
+        console.log('currentQuiz:', currentQuiz);
+
         currentQuiz.push(shuffledArray[i])
-        console.log(currentQuiz)
         const vocab = `
             <div class="vocabulary">
                 <img src="${shuffledArray[i].image}">
@@ -193,7 +249,7 @@ const setVocabulary = () => {
         vocabs.innerHTML = vocab     
         document.querySelectorAll('.choice').forEach(choice => {
             choice.addEventListener('click', () => {
-                if(choice.innerText == quiz[i].answer) {
+                if(choice.innerText == shuffledArray[i].answer) {
                     // showState.innerText = 'Ναί'
                     showCheck.innerText = '✔'
                     setTimeout(function() {
@@ -215,7 +271,7 @@ const setVocabulary = () => {
 
 const setNext = () => {
     i++
-    if (i < quiz.length) {
+    if (i < myQuiz.length) {
         console.log(`i is less than quiz length ${i}`)
         setVocabulary();
         
@@ -228,5 +284,5 @@ const setNext = () => {
 }
 
 
-setVocabulary()
+// setVocabulary()
 
