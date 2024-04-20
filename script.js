3807157dd6596eda0f1fd5707f683bfb67fb4fd8
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-04-20 13:05:42 +0800

    add

--- script.js
+++ script.js
@@ -266,7 +266,7 @@ const viewList = allVocabulary.map((list) => {
     const itemsHTML = vocabularyItems.map(item => `<div>${item.answer}</div><img class="image-list" src="${item.image}">`).join(''); // Map each item to HTML and join them
     
     return `
-        <div class="vocabulary-lessons">${vocabularyName}</div>
+        <h3 class="vocabulary-headings">${vocabularyName}</h3>
         <div class="vocabulary-items">${itemsHTML}</div>
     `;
     
