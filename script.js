d8529af3a349309a07c0f6540e3d3d8b99e204b0
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-05-06 17:31:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -478,13 +478,14 @@ chooseLessons.forEach(newVocabulary => {
 const viewList = allVocabulary.map((list) => {
     const vocabularyName = Object.keys(list)[0]; // Get the name of the vocabulary
     const vocabularyItems = Object.values(list)[0]; // Get the array of vocabulary items
-    const itemsHTML = vocabularyItems.map(item => `<div class="list-vocabulary-items-holder"><div>${item.answer}</div><img class="image-list" src="${item.image}"></div>`).join(''); // Map each item to HTML and join them
-    
-    return `
-    <details>
-        <summary class="list-vocabulary-headings">${vocabularyName}</summary>
-        <div class="list-vocabulary-items">${itemsHTML}</div>
-    </details>
+        const itemsHTML = vocabularyItems.map(item => `
+            <div class="list-vocabulary-items-holder"><div>${item.answer}</div><img class="image-list" src="${item.image}"></div>`).join(''); // Map each item to HTML and join them
+                
+            return `
+            <details>
+                <summary class="list-vocabulary-headings">${vocabularyName}</summary>
+                <div class="list-vocabulary-items">${itemsHTML}</div>
+            </details>
     `;
     
 })
